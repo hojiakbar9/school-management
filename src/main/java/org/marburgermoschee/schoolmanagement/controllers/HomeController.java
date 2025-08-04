98d861f631389afecd299f898271306d1b915b11
Khayitboev Khojiakbar <xayitbaevhojiakbar@gmail.com>
2025-08-04 15:37:39 +0200

    initial commit

---
+++ src/main/java/org/marburgermoschee/schoolmanagement/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package org.marburgermoschee.schoolmanagement.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping
+    public String welcome(){
+        return "This is school management app";
+    }
+}
