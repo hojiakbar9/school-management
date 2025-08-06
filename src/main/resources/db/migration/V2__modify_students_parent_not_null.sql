0f7d40879c0d037940713439bd41b2daa87b8468
Khayitboev Khojiakbar <xayitbaevhojiakbar@gmail.com>
2025-08-06 17:39:32 +0200

    update students table: redefine parent_id foreign key and enforce not null constraint

--- src/main/resources/db/migration/V2__modify_students_parent_not_null.sql
+++ src/main/resources/db/migration/V2__modify_students_parent_not_null.sql
@@ -1,3 +1,13 @@
 alter table students
+    drop foreign key students_ibfk_1;
+
+alter table students
+    add constraint students_ibfk_1
+        foreign key (parent_id) references parents (id);
+
+
+alter table students
     modify parent_id int not null;
+
+
 
