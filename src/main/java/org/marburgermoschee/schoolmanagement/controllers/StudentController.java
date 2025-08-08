f890853bdbc985cbc06731545bafdb1b554d2628
Khayitboev Khojiakbar <xayitbaevhojiakbar@gmail.com>
2025-08-08 20:29:10 +0200

    add getAttendances endpoint to retrieve attendance records for a specific student

--- src/main/java/org/marburgermoschee/schoolmanagement/controllers/StudentController.java
+++ src/main/java/org/marburgermoschee/schoolmanagement/controllers/StudentController.java
@@ -2,18 +2,22 @@ package org.marburgermoschee.schoolmanagement.controllers;
 
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.marburgermoschee.schoolmanagement.dtos.AttendanceDto;
 import org.marburgermoschee.schoolmanagement.dtos.RegisterStudentRequest;
 import org.marburgermoschee.schoolmanagement.dtos.StudentDto;
+import org.marburgermoschee.schoolmanagement.entities.Attendance;
 import org.marburgermoschee.schoolmanagement.entities.Parent;
 import org.marburgermoschee.schoolmanagement.entities.Student;
 import org.marburgermoschee.schoolmanagement.exceptions.StudentNotFoundException;
 import org.marburgermoschee.schoolmanagement.exceptions.UserNotFoundException;
+import org.marburgermoschee.schoolmanagement.mappers.AttendanceMapper;
 import org.marburgermoschee.schoolmanagement.mappers.StudentMapper;
 import org.marburgermoschee.schoolmanagement.repositories.ParentRepository;
 import org.marburgermoschee.schoolmanagement.repositories.StudentRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @AllArgsConstructor
@@ -22,6 +26,7 @@ public class StudentController {
     private final StudentRepository studentRepository;
     private final StudentMapper studentMapper;
     private final ParentRepository parentRepository;
+    private final AttendanceMapper attendanceMapper;
 
     @PostMapping
     public StudentDto register(@Valid @RequestBody RegisterStudentRequest request) {
@@ -47,6 +52,12 @@ public class StudentController {
         return studentMapper.toDto(student);
     }
 
+    @GetMapping("/{id}/attendance")
+    public List<AttendanceDto> getAttendances(@PathVariable Integer id){
+        Student student = studentRepository.findById(id).orElseThrow(StudentNotFoundException::new);
+        Set<Attendance> attendances = student.getAttendances();
+        return attendances.stream().map(attendanceMapper::toDto).toList();
+    }
     @PutMapping("/{id}")
     public StudentDto updateStudent(
             @Valid @RequestBody RegisterStudentRequest request,
@@ -64,5 +75,6 @@ public class StudentController {
         Student student = studentRepository.findById(id).orElseThrow(StudentNotFoundException::new);
         studentRepository.delete(student);
     }
+
 
 }
