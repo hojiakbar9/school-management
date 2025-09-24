c0f883e03ca61d3b0227cceb58c82e41b1e6d75a
Khayitboev Khojiakbar <xayitbaevhojiakbar@gmail.com>
2025-09-24 22:05:20 +0200

    add reacter router and define initial routes

--- school-management-client/src/App.tsx
+++ school-management-client/src/App.tsx
@@ -1,14 +1,24 @@
-import { useEffect, useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import StudentsPage from "./pages/StudentsPage";
+import TeachersPage from "./pages/TeachersPage"; // You'll create this later
+import HomePage from "./pages/HomePage"; // You'll create this later
 
 function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    fetch("/api/students")
-      .then((res) => res.json())
-      .then((data) => setMessage(JSON.stringify(data))); // stringify if it's an object/array
-  }, []);
-  return <p className="text-3xl font-bold m-4">{message}</p>;
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/students">Students</Link> |{" "}
+        <Link to="/teachers">Teachers</Link>
+      </nav>
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/students" element={<StudentsPage />} />
+          <Route path="/teachers" element={<TeachersPage />} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
 
 export default App;
---
+++ school-management-client/src/pages/TeachersPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TeachersPage = () => {
+  return <div>Teachers Page</div>;
+};
+
+export default TeachersPage;
